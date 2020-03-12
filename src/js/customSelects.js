8093f877912e752758a4c39c84c124da6658a82d
Артем Сафин <artemsafin.webdev@gmail.com>
2020-03-12 11:28:55 +0300

    Фиксы

--- src/js/customSelects.js
+++ src/js/customSelects.js
@@ -2,6 +2,7 @@ import 'select2';
 
 export default function() {
     $('.js-custom-select').select2({
-        minimumResultsForSearch: -1
+        minimumResultsForSearch: -1,
+        dropdownParent: $('.page-content')
     });
 }
