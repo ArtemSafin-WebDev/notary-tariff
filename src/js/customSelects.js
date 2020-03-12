0cd1dde2284fd5ddc37f57bdcca0e20dce0703df
Артем Сафин <artemsafin.webdev@gmail.com>
2020-03-12 16:18:19 +0300

    Правки селекта

--- src/js/customSelects.js
+++ src/js/customSelects.js
@@ -1,8 +1,15 @@
 import 'select2';
 
 export default function() {
-    $('.js-custom-select').select2({
-        minimumResultsForSearch: -1,
-        dropdownParent: $('.page-content')
+
+
+    $('.js-custom-select').each(function () {
+        let currentSelect = $(this);
+
+        currentSelect.select2({
+            minimumResultsForSearch: -1,
+            dropdownParent: currentSelect.parent()
+        });
     });
+    
 }
